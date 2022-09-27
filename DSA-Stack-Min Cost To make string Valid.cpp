d75a7b8e0fb7555cba46f2e89dd043e67b0f9f10
shreyas123gore <86887215+shreyas123gore@users.noreply.github.com>
2022-09-27 09:25:19 +0530

    Add files via upload

---
+++ DSA-Stack-Min Cost To make string Valid.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h> 
+int findMinimumCost(string str) 
+{
+  // Write your code here
+    stack<char>stk;
+    int c=0;
+    int ans=0;
+    
+    for(int i=0;i<str.length();i++)
+    {
+        if(str[i]=='{')
+        {
+            stk.push(str[i]);
+        }
+        else
+        {
+            if(!stk.empty())
+            {
+                stk.pop();
+            }
+            else
+            {
+                stk.push('{');
+                ans+=1;
+            }
+        }
+    }
+    
+    if(stk.empty())
+    {
+        return ans;
+    }  
+ 
+    while(!stk.empty())
+    {
+       c++;
+       stk.pop();
+    }
+    
+    if(c%2==0)
+    {
+        return (ans + c/2);
+    }
+    
+    return -1;
+}
