25482b5f5faed4356a1cb9ed7eefd9393bc8f46a
shreyas123gore <86887215+shreyas123gore@users.noreply.github.com>
2022-10-04 09:36:15 +0530

    Update Left Right Equal Sum.cpp

--- Basics/Arrays/Left Right Equal Sum.cpp
+++ Basics/Arrays/Left Right Equal Sum.cpp
@@ -1,1 +1,59 @@
+//https://practice.geeksforgeeks.org/problems/equal-sum0810/1
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    string equilibrium(int *arr, int n)
+    {
+        // code 
+        int totalsum=0;
+        for(int i=0;i<n;i++)
+        {
+           totalsum+=arr[i];
+        }
+        
+        int lsum=0;
+        int rsum=totalsum;
+        for(int i=0;i<n;i++)
+        {
+            rsum=rsum-arr[i];
+            
+            if(lsum==rsum)
+            {
+                return "YES";
+            }
+            
+            lsum+=arr[i];
+         
+           
+        }
+        
+        return "NO";
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.equilibrium(a, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
